dd435968b4867688c7384039145842d9cf42a637
ACE_0366 <rogax4179@gmail.com>
2026-02-04 19:45:44 -0600

    Inclusion de los archivos de el programa de reservaciones

---
+++ Reservación/Reservación/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reservación
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Reservación hecha para " + textBox1.Text + " " + textBox2.Text + " el día " + monthCalendar1.SelectionStart.ToShortDateString() + " en la habitación " + textBox3.Text;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+        }
+    }
+}
