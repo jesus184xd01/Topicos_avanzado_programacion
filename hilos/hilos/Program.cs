f7a1ad93f8a581a3d5dd9eb2a41f2a3b81e87fec
jesus184xd01 <jsvs.184xd01@gmail.com>
2026-03-07 19:11:34 -0600

    Codigo de ejecucion de un hilo
    Es la practica de clase con el codigo corregido

---
+++ hilos/hilos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace hilos
+{
+    internal class Program
+    {
+        // Metodo que imprime un mensaje
+        static void Trabajo()
+        {
+            // ejecuta un hilo/proceso para imprimir lo siguiente
+            Console.WriteLine("Hilo en ejecución");
+        } 
+
+        // Metodo principal que se ejecuta al iniciar la app
+        static void Main()
+        {
+            // Instancia de un objeto tipo hilo donde se le pasa como parametro el metodo Trabajo
+            Thread hilo = new Thread(Trabajo);
+            // Se inicia el hilo
+            hilo.Start();
+            // Imprime un mensaje de que el hilo ya esta ejecutandose
+            Console.WriteLine("Hilo principal en ejecución");
+        }
+    }
+}
